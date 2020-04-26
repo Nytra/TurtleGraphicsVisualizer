de5d3eedd6e7a647249645c1bd071de0567b0057
Sam Scott <samueltscott@gmail.com>
2020-04-26 18:53:03 +0100

    massively optimized rendering. added new gif showcasing 1000 small turtles

--- turtle.py
+++ turtle.py
@@ -21,12 +21,14 @@ SCREEN_HEIGHT = 600
 TURTLE_ACTION_INTERVAL = 0 # time in ms between actions
 TURTLE_PEN_COLOURS = RED, GREEN, BLUE
 TURTLE_SIZE_DEFAULT = 10
-TURTLE_MOVE_SPEED_DEFAULT = 10
+TURTLE_MOVE_SPEED_DEFAULT = 1
 TURTLE_PERFORM_CURRENT_DIRECTORY = True # perform all TurtleScript files in the current (relative) directory
 TURTLE_RANDOM_DIST_MAX = 10
 TURTLE_CAN_OVERLAP_TRAIL = True
 TURTLE_USES_FULL_COLOUR_RANGE = True
 TURTLE_USES_FULL_DIRECTION_RANGE = True
+TURTLE_NO_TRAIL = False
+RENDER_STATIC = True # don't clear the screen every frame, don't fill the screen every frame, only draw when needed
 NORTH_ANGLE = 0
 TO_RAD = 3.14 / 180.0
 
@@ -53,8 +55,10 @@ class Turtle:
         
         self.size = TURTLE_SIZE_DEFAULT
         self.moveSpeed = TURTLE_MOVE_SPEED_DEFAULT
-        self.x = random.randint(0, int(SCREEN_WIDTH / self.size) - 1) * self.size
-        self.y = random.randint(0, int(SCREEN_HEIGHT / self.size) - 1) * self.size
+        #self.x = random.randint(0, int(SCREEN_WIDTH / self.size) - 1) * self.size
+        #self.y = random.randint(0, int(SCREEN_HEIGHT / self.size) - 1) * self.size
+        self.x = random.randint(0, getHCells() - 1)
+        self.y = random.randint(0, getVCells() - 1)
         self.penDown = False
         self.penPtr = 0 # pen 1 = red, pen 2 = green, pen 3 = blue
         self.rgb = TURTLE_PEN_COLOURS[self.penPtr]
@@ -156,34 +160,61 @@ class Turtle:
     def remember(self, data):
         exists = False
 
-        for i in range(len(screenBuffer)):
-            if screenBuffer[i][0] == data[0] and screenBuffer[i][1] == data[1]:
-                exists = True
-                if screenBuffer[i][2] == data[2] and self.changeColourOnCollisionMode == 1:
+        x = data[0]
+        y = data[1]
+        colour = data[2]
+
+        if RENDER_STATIC:
+            pygame.draw.rect(screen, colour, (x * TURTLE_SIZE_DEFAULT, y * TURTLE_SIZE_DEFAULT, TURTLE_SIZE_DEFAULT, TURTLE_SIZE_DEFAULT))
+            #pygame.display.update()
+            return
+
+        cell = getCell(x, y)
+        if cell != None:
+            exists = True
+            if cell == colour and self.changeColourOnCollisionMode == 1 or self.changeColourOnCollisionMode == 2:
                     # if the turtle is the same colour as the screen cell it occupies
                     # change the colour to something else
-                    # (this way we don't lose track of where the turtle is)
-                    #temp = self.penPtr
                     self.penPtr = random.choice(list(n for n in range(0, len(TURTLE_PEN_COLOURS)) if n != self.penPtr))
                     if TURTLE_USES_FULL_COLOUR_RANGE:
-                        screenBuffer[i][2] = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+                        randColour = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+                        setCell(x, y, randColour)
                     else:
-                        screenBuffer[i][2] = TURTLE_PEN_COLOURS[self.penPtr]
-                elif self.changeColourOnCollisionMode == 2:
-                    #temp = self.penPtr
-                    #while self.penPtr == temp:
-                        #self.penPtr = random.randint(0, len(TURTLE_PEN_COLOURS) - 1)
-                    self.penPtr = random.choice(list(n for n in range(0, len(TURTLE_PEN_COLOURS)) if n != self.penPtr))
-                    if TURTLE_USES_FULL_COLOUR_RANGE:
-                        screenBuffer[i][2] = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
-                    else:
-                        screenBuffer[i][2] = TURTLE_PEN_COLOURS[self.penPtr]
-                else:
-                    screenBuffer[i][2] = data[2]
-                break
+                        setCell(x, y, TURTLE_PEN_COLOURS[self.penPtr])
+            else:
+                setCell(x, y, colour)
+            #elif self.changeColourOnCollisionMode == 2:
+
+###############
+        # for i in range(len(screenBuffer)):
+        #     if screenBuffer[i][0] == data[0] and screenBuffer[i][1] == data[1]:
+        #         exists = True
+        #         if screenBuffer[i][2] == data[2] and self.changeColourOnCollisionMode == 1:
+        #             # if the turtle is the same colour as the screen cell it occupies
+        #             # change the colour to something else
+        #             self.penPtr = random.choice(list(n for n in range(0, len(TURTLE_PEN_COLOURS)) if n != self.penPtr))
+        #             if TURTLE_USES_FULL_COLOUR_RANGE:
+        #                 randColour = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+        #                 setCell(x, y, randColour)
+        #             else:
+        #                 setCell(x, y, TURTLE_PEN_COLOURS[self.penPtr])
+
+                # elif self.changeColourOnCollisionMode == 2:
+                #     #temp = self.penPtr
+                #     #while self.penPtr == temp:
+                #         #self.penPtr = random.randint(0, len(TURTLE_PEN_COLOURS) - 1)
+                #     self.penPtr = random.choice(list(n for n in range(0, len(TURTLE_PEN_COLOURS)) if n != self.penPtr))
+                #     if TURTLE_USES_FULL_COLOUR_RANGE:
+                #         screenBuffer[i][2] = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+                #     else:
+                #         screenBuffer[i][2] = TURTLE_PEN_COLOURS[self.penPtr]
+                # else:
+                #     screenBuffer[i][2] = data[2]
+                # break
 
         if not exists:
-            screenBuffer.append(data)
+            #screenBuffer.append(data)
+            setCell(x, y, colour)
 
     def doNext(self):
         
@@ -257,11 +288,14 @@ class Turtle:
                     if TURTLE_USES_FULL_DIRECTION_RANGE:
                         self.direction = random.randint(0, 359)
                     
-                    x2 = self.x + (value * math.sin(self.direction * TO_RAD) * self.size)
-                    y2 = self.y + (value * (math.cos(self.direction * TO_RAD) * -1) * self.size)
+                    x2 = self.x + (value * math.sin(self.direction * TO_RAD)) #* self.size)
+                    y2 = self.y + (value * (math.cos(self.direction * TO_RAD) * -1)) #* self.size)
                     
-                    x2 = round(x2 / self.size) * self.size
-                    y2 = round(y2 / self.size) * self.size
+                    #x2 = round(x2 / self.size) * self.size
+                    #y2 = round(y2 / self.size) * self.size
+
+                    x2 = round(x2)
+                    y2 = round(y2)
                     
                     self.target = (x2, y2)
                     
@@ -299,28 +333,31 @@ class Turtle:
                             self.y += diff * sign
                         else:
                             self.y += self.moveSpeed * sign
+                    
+                    
 
-                    if [self.x, self.y] in list(cell[:2] for cell in screenBuffer) and TURTLE_CAN_OVERLAP_TRAIL == False:
-                        #print(screenBuffer[0][:2])
-                        self.x = prevx
-                        self.y = prevy
-                        self.actionPtr += 1
-                        self.hasDoneCurrentAction = False
-                        self.doNext()
+                    # if [self.x, self.y] in list(cell[:2] for cell in screenBuffer) and TURTLE_CAN_OVERLAP_TRAIL == False:
+                    #     #print(screenBuffer[0][:2])
+                    #     self.x = prevx
+                    #     self.y = prevy
+                    #     self.actionPtr += 1
+                    #     self.hasDoneCurrentAction = False
+                    #     self.doNext()
 
                 offScreen = False
+                
                 if self.x < 0:
                     self.x = 0
                     offScreen = True
-                elif self.x > SCREEN_WIDTH - self.size:
-                    self.x = SCREEN_WIDTH - self.size
+                elif self.x > getHCells() - 1:
+                    self.x = getHCells() - 1
                     offScreen = True
 
                 if self.y < 0:
                     self.y = 0
                     offScreen = True
-                elif self.y > SCREEN_HEIGHT - self.size:
-                    self.y = SCREEN_HEIGHT - self.size
+                elif self.y > getVCells() - 1:
+                    self.y = getVCells() - 1
                     offScreen = True
                 
                 if offScreen:
@@ -329,6 +366,13 @@ class Turtle:
                     self.doNext()
 
                 #print(self.x, self.y)
+
+                if getCell(self.x, self.y) != None and TURTLE_CAN_OVERLAP_TRAIL == False:
+                        self.x = prevx
+                        self.y = prevy
+                        self.actionPtr += 1
+                        self.hasDoneCurrentAction = False
+                        self.doNext()
                         
                 if self.penDown:
                     if TURTLE_USES_FULL_COLOUR_RANGE:
@@ -341,24 +385,75 @@ class Turtle:
                     self.hasDoneCurrentAction = False
                     self.doNext()
             
+def getCell(x, y):
+    #print((y * SCREEN_WIDTH) + x, len(screenBuffer))
+    #print(getCellIndex(x, y), len(screenBuffer))
+    return screenBuffer[getCellIndex(x, y)]
+
+def setCell(x, y, colour):
+    global screenBuffer
+    #print((y * SCREEN_WIDTH) + x, len(screenBuffer))
+    screenBuffer[getCellIndex(x, y)] = colour
+
+def getCellIndex(x, y):
+    hCells = getHCells()
+
+    #print("raw ", x, y)
+    #print("idx ", (y * (hCells)) + x)
+    #print("len ", len(screenBuffer))
+
+    return (y * (hCells)) + x
+
+def getCellPos(i):
+    y = (i // getHCells()) - 1
+    x = i - (y * getHCells()) 
+    #print("i", i, "x", x, "y", y)
+
+    return x, y
+
+def getHCells():
+    return int(SCREEN_WIDTH / TURTLE_SIZE_DEFAULT)
+
+def getVCells():
+    return int(SCREEN_HEIGHT / TURTLE_SIZE_DEFAULT)
 
 def display():
     pygameQuit = False
     timerStart = pygame.time.get_ticks()
     hasDoneInitialPause = True
     #initialDelay = 1000 # specify delay to add when program starts. before turtle starts performing actions
-    
+    #frameTime = 0
+
+    #getCellPos(getCellIndex(100, 100))
+    #input()
+
+    #screen.flip()
+    if RENDER_STATIC:
+        screen.fill(BG_COLOUR)
+        #screen.unlock()
+        #for i in range(len(screenBuffer)):
+            #pygame.draw.rect(screen, screenBuffer[i], (getCellPos(i)[0] * TURTLE_SIZE_DEFAULT, getCellPos(i)[1] * TURTLE_SIZE_DEFAULT, TURTLE_SIZE_DEFAULT, TURTLE_SIZE_DEFAULT))
+        #pygame.display.flip()
+        #screen.lock()
+        pygame.display.update()
+
     while not pygameQuit:
+        frameStart = pygame.time.get_ticks()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 #pygame.quit()
                 pygameQuit = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    screen.fill(BG_COLOUR)
 
         if pygame.time.get_ticks() - timerStart < 15000 and hasDoneInitialPause == False:
             continue
         hasDoneInitialPause = True
 
-        screen.fill(BG_COLOUR)
+        # maybe don't clear the screen to get better performance?
+        if not RENDER_STATIC:
+            screen.fill(BG_COLOUR)
 
         # logic here
         if pygame.time.get_ticks() - timerStart > TURTLE_ACTION_INTERVAL:
@@ -367,13 +462,28 @@ def display():
                 t.doNext()
 
         #for t in turtles:
-        for element in screenBuffer:
-            pygame.draw.rect(screen, element[2], (element[0], element[1], TURTLE_SIZE_DEFAULT, TURTLE_SIZE_DEFAULT))
+        if not RENDER_STATIC:
+            if TURTLE_NO_TRAIL == False:
+                for y in range(getVCells()):
+                    for x in range(getHCells()):
+                        if getCell(x, y) != None:
+                            pygame.draw.rect(screen, getCell(x, y), (x * TURTLE_SIZE_DEFAULT, y * TURTLE_SIZE_DEFAULT, TURTLE_SIZE_DEFAULT, TURTLE_SIZE_DEFAULT))
+            else:
+                for t in turtles:
+                    pygame.draw.rect(screen, t.rgb, (t.x * TURTLE_SIZE_DEFAULT, t.y * TURTLE_SIZE_DEFAULT, TURTLE_SIZE_DEFAULT, TURTLE_SIZE_DEFAULT))
+
+        #for element in screenBuffer:
+            #pygame.draw.rect(screen, element[2], (element[0], element[1], TURTLE_SIZE_DEFAULT, TURTLE_SIZE_DEFAULT))
             #pygame.draw.circle(screen, TURTLE_PEN_COLOURS[element[2]], (element[0], element[1]), TURTLE_SIZE_DEFAULT)
 
+        #if not RENDER_STATIC:
         pygame.display.update()
 
         clock.tick(frameRate)
+        frameEnd = pygame.time.get_ticks()
+        frameTime = frameEnd - frameStart
+        pygame.display.set_caption("FrameTime: " + str(frameTime) + " FPS: " + str(int(1000 / frameTime)))
+
 
     pygame.quit()
     #sys.exit()
@@ -417,15 +527,32 @@ def waitForValidFileName():
             print("error: that file does not exist in the current directory")
     return inp
 
+def clearScreenBuffer():
+    global screenBuffer
+    screenBuffer = []
+
+    hCells = getHCells() # maybe math.floor() ?
+    vCells = getVCells()
+    print("setting", hCells * vCells, "cells")
+
+    for i in range(hCells * vCells):
+        if not RENDER_STATIC:
+            screenBuffer.append(None)
+        else:
+            screenBuffer.append(BG_COLOUR)
+
 
 flags = HWSURFACE | DOUBLEBUF
 bpp = 16
 
 clock = pygame.time.Clock()
-frameRate = 120
+frameRate = 999
 
 turtles = []
 screenBuffer = []
+
+clearScreenBuffer()
+
 options = [
     ("Perform a specific TurtleScript file", "readone"),
     ("Perform all TurtleScript files in the current directory (simultaneously)", "readall"),
@@ -531,7 +658,8 @@ while not quitGame:
         pygame.init()
         pygame.display.set_caption("TurtleScript Drawing Program")
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), flags, bpp)
-        screenBuffer = []
+        
+        clearScreenBuffer()
 
         display()
     
